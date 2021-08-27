8cd8b207fa061a84a66ffbfc011e07d2c1e12534
kiryu500 <86351139+kiryu500@users.noreply.github.com>
2021-08-27 20:46:40 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,9 +23,6 @@ $('.slide-L').on('inview', function(event, isInView, visiblePartX, visiblePartY)
   });
 });
 
-$('.nav_toggle').on('click', function () {
-  $('.nav_toggle, .nav').toggleClass('show');
-});
 
 //TOP切り替え
 (function(){
